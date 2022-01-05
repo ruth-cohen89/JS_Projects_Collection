c62569e78ee5959b6087f62b4aeaed45ee35289a
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-01-05 08:41:40 +0200

    ES6 Version

---
+++ JS-Projects/BookList/appES6.js
@@ -0,0 +1,109 @@
+// Book Constructor
+class Book{
+    constructor (title, author, isbn){
+    this.title=title;
+    this.author=author;
+    this.isbn=isbn;
+    }
+}
+
+//UI Constructor
+//In this version we put the prototype methods inside the class
+class UI{
+
+    //Add book to list
+    addBookToList(book){
+        const list = document.getElementById('book-list');
+        //create tr element
+        const row=document.createElement('tr');
+        //Insert cols
+        row.innerHTML= `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td><a href="#" class="delete">X<a></td>
+        `;
+    
+        list.appendChild(row);           
+    }
+    
+    //Delete Book
+    deleteBook(target){
+    if(target.className==='delete'){
+        target.parentElement.parentElement.remove();
+      }
+    }
+
+    //Clear fields
+    clearFields(){
+    document.getElementById('title').value='';
+    document.getElementById('author').value='';
+    document.getElementById('isbn').value='';
+    }
+
+    //Show alert
+    showAlert(msg,className){
+    //Create div
+    const div=document.createElement('div');
+    //Add classes
+    div.className=`alert ${className}`;
+    //Add text
+    div.appendChild(document.createTextNode(msg));
+    //Get parent (when we want to insert an element- we have to insert it before a father's child)
+    const container=document.querySelector('.container');
+    const form=document.querySelector('#book-form');
+    //Insert alert
+    container.insertBefore(div, form)
+    //TimeOut after 3 sec
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+       },3000)
+    }
+
+}
+
+
+//Event Listener
+document.getElementById('book-form').addEventListener('submit',function(e){
+    //Get form values
+    const title=document.getElementById('title').value;
+          author=document.getElementById('author').value;
+          isbn=document.getElementById('isbn').value;
+    
+    //Instantiate book
+    const book= new Book(title,author,isbn);
+
+    //Instantiate UI 
+    const ui= new UI();
+
+    //Validate
+    if(title===''|| author===''||isbn===''){
+        //Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+    
+    } else {
+    //Add book to list
+    ui.addBookToList(book);
+    
+    //Show success
+    ui.showAlert('Book Added!', 'success')
+
+    //Clear fields
+    ui.clearFields('Please fill on all fields');
+    }
+
+    e.preventDefault();
+});
+
+//Event listener for delete
+document.getElementById('book-list').addEventListener('click',function(e){
+    //Instantiate the UI (In order to use its prototype methods)
+    const ui=new UI();
+
+    //Delete book
+    ui.deleteBook(e.target);
+
+    //Show alert
+    ui.showAlert('Book removed!', 'success');
+     
+    e.preventDefault();
+});
