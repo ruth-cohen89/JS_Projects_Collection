860e43facdba8dad72a5d7af5896609e5a8779ca
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-01-11 13:27:26 +0200

    Show alert message
    API rate limit exceeded

--- JS-Projects/API_Projects/githubfinder/app.js
+++ JS-Projects/API_Projects/githubfinder/app.js
@@ -10,27 +10,23 @@ const searchUser=document.getElementById('searchUser');
 searchUser.addEventListener('keyup',(e)=>{
     //Get input text
     const userText=e.target.value;
-
+  
     if(userText.text!==''){
         //Make http calls
         gitHub.getUser(userText)
         .then(data=>{
             if(data.profile.message==='Not Found'){
                 //Show alert
-
+                ui.showAlert('user not found', 'alert alert-danger')
             }else{
                 //Show profile
                 ui.showProfile(data.profile);
             }
-        })
-    }else{
-        //Clear profile
-
+      })
+    } else {
+    // Clear profile
+    ui.clearProfile();
     }
+    
 });
 
-//Search user
-function search(e){
-    
-    //e.preventDefault();
-}
--- JS-Projects/API_Projects/githubfinder/github.js
+++ JS-Projects/API_Projects/githubfinder/github.js
@@ -2,14 +2,21 @@ class GitHub{
     constructor(){
         //The API key is in case we make more than a 100 requests per hour
         this.client_id='9602210b6cfe538170a3';
-        this.client_secret='5bb8db94a14e5a09114f532b2a808f662a2df794';
+        this.client_secret='e55734006135e123d000208e36c63e3dfd3605b2';
     }
+//async await with fetch
+// async function fetchUsers(){
+//     const res=await fetch('https://jsonplaceholder.typicode.com/users');
+//     console.log(res)
+//     const data=await res.json();
 
+//     console.log(data)
+// }
+// fetchUsers();
     async getUser(user){
-        const profileResponse=await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-        
+       
+        const profileResponse= await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
         const profile=await profileResponse.json();
-        
         return{
             profile
         }
