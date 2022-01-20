0a8adf5f017e54447cb9cea6a38bac210102a9e2
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-01-20 11:24:59 +0200

    Observer Pattern App
    subscribe/unsubscribe
    to get-ms/get-s

---
+++ JS-Projects/observerPattern/appEs6.js
@@ -0,0 +1,59 @@
+//Constructor
+class EventObserver{
+    constructor(){
+        this.observers=[];
+    }
+
+    subscribe(fn){
+        this.observers.push(fn);
+        console.log(`Subscribed to ${fn.name}`);
+    }
+
+    unsubscribe(fn){
+        this.observers=this.observers.filter((item)=>{
+          if(item!==fn){
+            return item;}
+        });     
+        console.log(`Unsubscribed to ${fn.name}`)
+    }
+
+    fire(){
+        this.observers.forEach((item)=>{
+            item.call();
+    });
+}
+}
+
+
+const click=new EventObserver();
+
+//Event listeners
+document.querySelector('.sub-ms').addEventListener('click',()=>{
+    click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector('.unSub-ms').addEventListener('click',()=>{
+    click.unsubscribe(getCurMilliseconds);
+
+});
+document.querySelector('.sub-s').addEventListener('click',()=>{
+    click.subscribe(getCurSeconds);
+});
+
+document.querySelector('.unSub-s').addEventListener('click',()=>{
+    click.unsubscribe(getCurSeconds);
+
+});
+
+document.querySelector('.fire').addEventListener('click',()=>{
+    click.fire();
+});
+
+//Click handlers, (the user can subscribe or an subscribe to this 'service')
+const getCurMilliseconds=()=>{
+    console.log(`current milliseconds: ${new Date().getMilliseconds()}`);
+}
+
+const getCurSeconds=()=>{
+    console.log(`current seconds: ${new Date().getSeconds()}`);
+}
