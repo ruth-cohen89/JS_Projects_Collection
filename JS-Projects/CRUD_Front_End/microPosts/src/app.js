def70fa5eece819da93dd9711dd899a48883bc23
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-01-24 21:29:48 +0200

    done :)

--- JS-Projects/CRUD_Front_End/microPosts/src/app.js
+++ JS-Projects/CRUD_Front_End/microPosts/src/app.js
@@ -18,12 +18,17 @@ document.addEventListener('DOMContentLoaded', getPosts);
 document.querySelector('.post-submit').addEventListener('click',
  submitPost);
 
-//  ui.postSubmit.addEventListener('click',
-//  addPost);
-
-//Listen for delete and edit post
+//Listen for delete post
 document.querySelector('#posts').addEventListener('click',
- changePost);
+deletePost);
+
+//Listen for enable edit post
+document.querySelector('#posts').addEventListener('click',
+enableEdit);
+
+//Listen for cancel
+document.querySelector('.card-form').addEventListener('click',
+cancelEdit)
 
 function getPosts(){
     http.get('http://localhost:3000/posts')
@@ -35,28 +40,85 @@ function getPosts(){
 function submitPost(){
     const title=document.querySelector('#title').value;
     const body=document.querySelector('#body').value;
-    
+    const id=document.querySelector('#id').value;
+
     const data={
         title,
         body
     }
 
-    //Create post
-    http.post('http://localhost:3000/posts', data)
+    //Validate input
+    if(title===''||body===''){
+        ui.showAlert('Please fill in all fields', 'alert alert-danger');
+    } else {
+     //Check for ID (hidden field)
+     if(id===''){
+     
+       //Create post
+       http.post('http://localhost:3000/posts', data)
+        .then((posts)=>{
+          ui.showAlert('Post added', 'alert alert-success');
+          ui.clearFields();
+          getPosts(posts);
+        })
+        .catch(err=> console.log(err));
+   } else {
+    //Update post
+    http.put(`http://localhost:3000/posts/${id}`, data)
     .then((posts)=>{
-        ui.showAlert('Post added', 'alert alert-success');
-        ui.clearFields();
-        getPosts(posts);
+      ui.showAlert('Post updated', 'alert alert-success');
+      ui.changeFormState('add');
+      getPosts(posts);
     })
     .catch(err=> console.log(err));
+
+}
+    }
 }
 
-function changePost(e){
-    if(e.target.class==='delete'){
-        console.log(target.parent.parent)
-        // let post=target.parent.parent.value.id;
-        // http.delete('http://localhost:3000/posts')
+function deletePost(e){
+    if(e.target.parentElement.classList.contains('delete')){
+        const id=e.target.parentElement.dataset.id;
+        if(confirm('Are you sure?')){
+            
+            http.delete(`http://localhost:3000/posts/${id}`)
+            .then(data=> {
+              ui.showAlert('Post Removed', 'alert alert-success');
+            getPosts();
+            })
+            .catch(err=>console.log(err));
+        }
+
     }
 
     e.preventDefault(); 
+}
+
+
+//Enable Edit State
+function enableEdit(e){
+    if(e.target.parentElement.classList.contains('edit')){
+      const id=e.target.parentElement.dataset.id;
+      const title=e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+      const body=e.target.parentElement.previousElementSibling.textContent;
+
+      const data={
+          id,
+          title,
+          body
+      }
+
+      //Fill from
+      ui.fillForm(data);
+    }
+    e.preventDefault()
+}
+
+//Cancel edit state
+function cancelEdit(e){
+  if(e.target.classList.contains('post-cancel')){
+      ui.changeFormState('add');
+  }
+
+  e.preventDefault();
 }
--- JS-Projects/CRUD_Front_End/microPosts/src/ui.js
+++ JS-Projects/CRUD_Front_End/microPosts/src/ui.js
@@ -12,23 +12,21 @@ class UI{
     let output='';
 
     posts.forEach((post)=>{
-        output+=`
+        output += `
         <div class="card mb-3">
-          <div class="card-body>
-           <h4 class="card-title">${post.title}</h4>
-           <p class="card-text">${post.body}</p>
-           <a href="#" class="edit card-link" 
-            data-id="${post.id}">
-            <i class="fa fa-pencil"></i>
-           </a>
+          <div class="card-body">
+            <h4 class="card-title">${post.title}</h4>
+            <p class="card-text">${post.body}</p>
+            <a href="#" class="edit card-link" data-id="${post.id}">
+              <i class="fa fa-pencil"></i>
+            </a>
 
-           <a href="#" class="delete card-link" 
-           data-id="${post.id}">
-           <i class="fa fa-remove"></i>
+            <a href="#" class="delete card-link" data-id="${post.id}">
+            <i class="fa fa-remove"></i>
           </a>
-         </div>
+          </div>
         </div>
-        `;
+      `;
     });
     this.post.innerHTML=output;
 
@@ -67,6 +65,49 @@ class UI{
       this.titleInput.value='';
       this.bodyInput.value='';
     }
+    // Fill form to edit
+    fillForm(data) {
+     this.titleInput.value = data.title;
+     this.bodyInput.value = data.body;
+     this.idInput.value = data.id;
+
+     this.changeFormState('edit');
+  }
+  //Clear ID hidden value
+  clearIdInput(){
+      this.idInput.value='';
+  }
+  //Change form state
+  changeFormState(type){
+      if(type==='edit'){
+        this.postSubmit.textContent='Update Post';
+        this.postSubmit.className='post-submit btn btn-warning btn-block';
+        
+        //Create cancel button
+        const button=document.createElement('button');
+        button.className='post-cancel btn btn-light btn-block';
+        button.appendChild(document.createTextNode('Cancel Edit'));
+
+        //Get parent
+        const cardForm=document.querySelector('.card-form');
+        //Get element to insert before
+        const formEnd=document.querySelector('.form-end');
+        //Insert cancel button
+        cardForm.insertBefore(button,formEnd );
+
+    } else {
+        this.postSubmit.textContent='Post it';
+        this.postSubmit.className='post-submit btn btn-primary';
+        //Remove cancel btn if it is there
+        if(document.querySelector('.post-cancel')){
+            document.querySelector('.post-cancel').remove();
+        }
+        //Clear ID from hidden field
+        this.clearIdInput();
+        //Clear text
+        this.clearFields();
+      }
+  }
 }
 
 export const ui= new UI();
