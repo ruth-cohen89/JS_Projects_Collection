268a0e3ff87516081678ebb27e3ee52617555dbc
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-02-01 22:32:16 +0200

    Streams
    Learning
    the 3 method- pipe operator is the best for consuming and writing streams,
    unless we  need a more customized solution so we can use 1 or 2 solutions I commented.

--- NodeJS_Projects/2-HOW-NODE-WORKS/streams.js
+++ NodeJS_Projects/2-HOW-NODE-WORKS/streams.js
@@ -1,4 +1,34 @@
 const fs =require('fs');
-const server=reuqire('http').createServer();
+const server=require('http').createServer();
 
-server.on('request')
+server.on('request', (req,res)=>{
+  //Solution 1, the file is inserted into a variable: "data", stored and then sent
+//   fs.readFile('test-file.txt', (err,data)=>{
+//     if(err) console.log(err);
+//     res.end(data);
+//   });
+
+  //Solution 2: Streams,
+//   const readable = fs.createReadStream('tesst-file.txt');
+//   readable.on('data', chunk=>{
+//       res.write(chunk);
+//   })
+//   readable.on('end', ()=>{
+//       res.end();
+//   })
+//   readable.on('error', err=>{
+//     console.log(err)
+//     res.statusCode = 500;
+//     res.end('File not found!')
+//   })
+
+  //Solution 3, pipe
+  const readable = fs.createReadStream('test-file.txt');
+  readable.pipe(res)
+  //explanation: readableSource.pipe(writableDest)
+});
+
+//start the server
+server.listen(8000, "127.0.0.1", ()=>{
+    console.log('Listening bitch...')
+});
