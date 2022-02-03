be57b1a4065e314fa990b3f3aefe0926cf2b98a7
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-02-03 23:50:47 +0200

    New Project - building an API

---
+++ NodeJS_Projects/4-Natours/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+app.listen(port, ()=>{
+   console.log(`App running on port ${port}...`);
+})
+//routes bitch (●'◡'●)😍😘🥰
