5a0479c5946d45e150e4e99dcc87c8fb2032f3de
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-02-11 00:15:51 +0200

    More Documentation

--- NodeJS_Projects/4-Natours/routes/tourRoutes.js
+++ NodeJS_Projects/4-Natours/routes/tourRoutes.js
@@ -1,14 +1,17 @@
 //The router
 const express = require('express');
 
-//??
+//Requiring tour controller
 const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
 //Routing to the next middleware
+
+//If URL contains id parameter, then use checkID MW
 router.param('id', tourController.checkID);
 
+//Now, determine the next middleWare
 router
   .route('/')
   .get(tourController.getAllTours)
--- NodeJS_Projects/4-Natours/server.js
+++ NodeJS_Projects/4-Natours/server.js
@@ -4,12 +4,26 @@ const dotenv = require('dotenv');
 //Config env vars, before reading app module
 dotenv.config({ path: './config.env' });
 
+//Require app, which startes the server
 const app = require('./app');
 
 //console.log(process.env);
 
 const port = process.env.PORT || 3000;
 
+//Listen to the server
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+//Natours:
+//1)start with server.js
+//creating a server and listen for incoming requests
+//2)app.js (application-level middleware )
+//and mounting the router that matches the mount path
+//3) ...Router.js, (router-level middleware),
+//and pile matching middlewares
+//4) ...Controller.js , use piles mw and send back the reponse (in the last mw)
+
+//Actually, in this project we create a web server (server.js)
+//and an API that handles the requests(routes & controllers)
