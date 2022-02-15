6b3b597bde718726c5be2a040274dc47e2114487
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-02-15 10:04:10 +0200

    Implemented all Mongoose

--- NodeJS_Projects/4-Natours/routes/tourRoutes.js
+++ NodeJS_Projects/4-Natours/routes/tourRoutes.js
@@ -13,6 +13,13 @@ const router = express.Router();
 
 //Now, determine the next middleWare
 router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
