4c148f96bdbf25c3113caf55199db46fd220ef4b
ruth-cohen89 <57313608+ruth-cohen89@users.noreply.github.com>
2022-02-18 13:08:29 +0200

    h

--- NodeJS_Projects/4-Natours/routes/tourRoutes.js
+++ NodeJS_Projects/4-Natours/routes/tourRoutes.js
@@ -5,6 +5,7 @@ const express = require('express');
 const tourController = require('../controllers/tourController');
 
 const router = express.Router();
+const authController = require('../controllers/authController');
 
 //Routing to the next middleware
 
@@ -18,7 +19,7 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 //If URL contains id parameter,
